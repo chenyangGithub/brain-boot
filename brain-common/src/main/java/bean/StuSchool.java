f3b4dd6f5d7a6fa0049e2ef9e86bfeec654db08b
chenyang <chenyang@meix.com>
2020-07-27 14:48:40 +0800

    user1 seccond commit

---
+++ brain-common/src/main/java/bean/StuSchool.java
@@ -0,0 +1,5 @@
+package bean;
+
+public class StuSchool {
+    private String name;
+}
--- brain-common/src/main/java/bean/Student.java
+++ brain-common/src/main/java/bean/Student.java
@@ -7,4 +7,5 @@ public class Student {
     private int id;
     private String name;
     private int age;
+    private String sex;
 }
